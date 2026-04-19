d055f18ca96bc83021dfb24e21c9bb1ba983ed4b
VILLALV <ianvillalobosalvarez@gmail.com>
2026-04-18 23:25:15 -0600

    Ian Villalobos A
    v5

--- TiendaTech/docker/app/sql/Tablas/Permisos.sql
+++ TiendaTech/docker/app/sql/Tablas/Permisos.sql
@@ -42,3 +42,5 @@ GRANT SELECT ON AdminProyecto.CATEGORIAS TO Operador;
 GRANT SELECT ON AdminProyecto.PRODUCTO_ATRIBUTOS TO Operador;
 
 GRANT SELECT ON AdminProyecto.VENTAS TO Consulta;
+
+GRANT SELECT ON AdminProyecto.REGISTRO TO Consulta;
